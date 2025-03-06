46c8896f02254869affa729800d8ae85f79699e8
Brahan Karthikeyan <brahan@gmail.com>
2025-03-06 19:17:14 +0530

    Create debug.h

---
+++ reference/c++/debug.h
@@ -0,0 +1,21 @@
+#ifndef ___DEBUG_H___
+# define ___DEBUG_H___
+
+#include <iostream>
+#include <string>
+
+std::string timestamp();
+
+#define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)    // only show filename and not it's path (less clutter)
+#define INFO std::cout << "[INFO] [" << timestamp().c_str() << "] " << __FILENAME__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
+#define WARN std::cout << "[WARN] [" << timestamp().c_str() << "] " << __FILENAME__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
+#define ERR std::cout << "[ERROR] [" << timestamp().c_str() << "] " << __FILENAME__ << "(" << __FUNCTION__ << ":" << __LINE__ << ") >> "
+
+void printStackTrace();
+
+#define STACK_TRACE printStackTrace();
+
+void printLoadedSharedLibraries();
+
+
+#endif // ___DEBUG_H___
